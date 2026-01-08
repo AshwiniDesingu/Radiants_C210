7b6b78b13ea335945337ee79ce846fa71183d874
AshwiniDesingu <ashwinidesingu635@gmail.com>
2026-01-08 11:10:10 +0530

    Add files via upload

--- popup.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("send").onclick = () => {
-  const query = document.getElementById("query").value;
-
-  chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-    chrome.tabs.sendMessage(tabs[0].id, { userQuery: query });
-  });
-};
